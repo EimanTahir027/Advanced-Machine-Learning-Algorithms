d36cdb644a35d57ab6129116b8c9021b6dae026a
7007259Ankur <ankurkumar7753@gmail.com>
2026-08-29 11:41:10 +0530

    fix:small but imp

--- code_files/2- Bagging and Random Forests/day2_ex.py
+++ code_files/2- Bagging and Random Forests/day2_ex.py
@@ -31,18 +31,19 @@ print("\n Classification Report: \n", classification_report(y_test, y_pred))
 param_grid = {
     'n_estimators': [50, 100, 200],
     'max_depth': [None, 10, 20],
-    'max_features': ['sqrt', 'log2', 'None']
+    'max_features': ['sqrt', 'log2', None]
 }
 
-grid_search = GridSearchCV(
-    estimator=RandomForestClassifier(random_state=42),
-    param_grid=param_grid,
-    cv=5,
-    scoring='accuracy',
-    n_jobs=-1
-)
-grid_search.fit(X_train, y_train)
+if __name__ == '__main__':
+    grid_search = GridSearchCV(
+        estimator=RandomForestClassifier(random_state=42),
+        param_grid=param_grid,
+        cv=5,
+        scoring='accuracy',
+        n_jobs=-1
+    )
+    grid_search.fit(X_train, y_train)
 
-# Display best parameters and score
-print(f"Best Parameters: {grid_search.best_params_}")
-print(f"Best Cross-Validation Accuracy: {grid_search.best_score_}")
+    # Display best parameters and score
+    print(f"Best Parameters: {grid_search.best_params_}")
+    print(f"Best Cross-Validation Accuracy: {grid_search.best_score_}")
